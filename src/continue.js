91f243d56dc3afde67a63710d1bfa43636cc3a1f
amaeljampen <amael.jampen@eduvaud.ch>
2024-09-13 14:39:43 +0200

    feat: continue better

--- src/continue.js
+++ src/continue.js
@@ -1,8 +1,14 @@
 const numbers = [];
+let somme = 0;
+
+
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
+    somme += numbers[i];
 }
+
+console.log(somme);
