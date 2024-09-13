e84d9c0fd4c60ecebb72a1a50ee96462ec6442b7
amaeljampen <amael.jampen@eduvaud.ch>
2024-09-13 14:26:29 +0200

    feat: break on time

--- src/break.js
+++ src/break.js
@@ -1,8 +1,17 @@
 const numbers = [];
-for (let i = 0; i < 10; i++) {
+let NbValInf = 0;
+    for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
 for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
+
+    if (numbers[i] > 50)
+    {
+        console.log(numbers[i]);
+        NbValInf += (numbers.length - i - 1);
+        console.log("Nombre de valeurs en dessous de 50 : " + NbValInf);
+        break; //contrôle de flux
+    }
+
 }
